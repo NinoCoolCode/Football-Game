7a35f98e66bddbba0c53c52fbcb5e4db8e44ec66
NinoCoolCode <142159777+NinoCoolCode@users.noreply.github.com>
2024-01-17 19:38:28 +0100

    Erster Commit

---
+++ Solo Projekt/Assets/FootballPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballPlayer : MonoBehaviour
+{
+    [SerializeField] private int playerSpeed = 5;
+    [SerializeField] private Transform gruondCheck;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ySpeed = rb.velocity.y;
+       RaycastHit2D hit= Physics2D.Raycast(gruondCheck.position,Vector2.down,0.1f);
+        
+        if (Input.GetKeyDown(KeyCode.Space)&&hit.collider!=null)
+        {
+            ySpeed = 5;
+        }
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*playerSpeed, ySpeed);   
+    }
+}
