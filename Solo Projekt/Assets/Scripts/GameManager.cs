84542c569807b8ef0abf74d3c50d55a949c10103
NinoCoolCode <142159777+NinoCoolCode@users.noreply.github.com>
2024-03-13 20:30:42 +0100

    start menu 2 Player

--- Solo Projekt/Assets/Scripts/GameManager.cs
+++ Solo Projekt/Assets/Scripts/GameManager.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private PlayerController playerController;
     [SerializeField] private GameObject menu;
     [SerializeField] private AiControler aiControler;
-    public void  Set1Player()
+    private void Awake()
+    {
+        Time.timeScale = 0;
+    }
+    public void Set1Player()
     {
         aiControler.enabled = true;
         menu.SetActive(false);
+        Time.timeScale = 1;
+
     }
+
+    public void Set2Player()
+    {
+        playerController.enabled = true;
+
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
 }
