ed53f2d8872256755e41057ba4ddfd74d6008ecb
NinoCoolCode <142159777+NinoCoolCode@users.noreply.github.com>
2024-03-20 21:00:12 +0100

    bild wechsel bei ball

--- Solo Projekt/Assets/Scripts/Ball.cs
+++ Solo Projekt/Assets/Scripts/Ball.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BallType
+{
+normal,
+robo
+
+}
+
 public class Ball : MonoBehaviour
 
 {
     [SerializeField] private Sprite roboBall;
-   
+   [SerializeField] private Sprite football;
+
     public static Ball Instance;
     private Vector3 startPosition;
 
     public void Reset()
     {
         transform.position = startPosition;
+        ChangeBall(BallType.normal);
         GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
         GetComponent<Rigidbody2D>().angularVelocity = 0;
     }
     public void SetVelocity(Vector2 direction)
     {
         GetComponent<Rigidbody2D>().velocity = direction;
+        ChangeBall(BallType.robo);
+        transform.rotation = Quaternion.identity;
+    }
+    public void ChangeBall(BallType ballType)
+    {
+        switch (ballType)
+        {
+            case BallType.normal:
+                GetComponent<SpriteRenderer>().sprite = football;
+                break;
+
+            case BallType.robo:
+                GetComponent<SpriteRenderer>().sprite = roboBall;
+                break;
+        }
     }
     private void Awake()
     {
@@ -28,7 +52,8 @@ public class Ball : MonoBehaviour
     void Start()
     {
         startPosition = transform.position;
-        GetComponent<SpriteRenderer>().sprite = roboBall;
+       football = GetComponent<SpriteRenderer>().sprite ;
+
     }
 
     // Update is called once per frame
