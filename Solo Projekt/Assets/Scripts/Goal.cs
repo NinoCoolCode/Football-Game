db4f93c473ceba697b1d2ca0ae8bb5eefab3ef56
NinoCoolCode <142159777+NinoCoolCode@users.noreply.github.com>
2024-04-10 20:00:43 +0200

    score Text

--- Solo Projekt/Assets/Scripts/Goal.cs
+++ Solo Projekt/Assets/Scripts/Goal.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
- [SerializeField]  private int goals;
+   private int goals;
+    [SerializeField] private TMP_Text goalText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Ball ballScript))
         {
             Ball.Reset();
             goals++;
+            goalText.text = goals.ToString();
 
             foreach (FootballPlayer player in FindObjectsOfType<FootballPlayer>())
             {
                 player.Reset();
             }
-           
-        }
+        }           
     }
 }
